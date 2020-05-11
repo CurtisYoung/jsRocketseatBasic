138b3c96b5fe7c1a2d6a6154408ac952f27b4210
Curtis Young <curtisyg@gmail.com>
2020-05-11 16:46:11 -0300

    Update With Functions
    Funçoes criadas para renderizar lista inicial de Todos, criar novo item todo a lista e excluir o item.

--- todos.js
+++ todos.js
@@ -7,3 +7,41 @@ var todos=[
     'Estudar JavaScript',
     'Acessar comunidade RocketSeat'
 ];
+
+function renderTodos(){
+    listElement.innerHTML = '';
+
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        var position = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+ position+')');
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+        todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+        listElement.appendChild(todoElement);
+        
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = ' ';
+    renderTodos();
+    
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    renderTodos();
+}
